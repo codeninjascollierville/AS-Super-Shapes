eccb146b8cef3d8a8f3bca6b34b7cdebb5bdb41b
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-18 12:28:29 -0600

    debugs fixed

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -7,32 +7,30 @@ public class Shape : MonoBehaviour
 [Header("Rigidbody Object")]
 public Rigidbody2D rb;
 [Header("Default Shrinking Speed")]
-public float shrinkspeed = 3f;
-}
+public float shrinkSpeed = 3f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-     rb.roation = Random.Range(0f, 360f);
-     transform.localScale = vector3.one * 10f;
+     rb.rotation = Random.Range(0f, 360f);
+     transform.localScale = Vector3.one * 10f;
     }
     
     // Update is called once per frame
     void Update()
     {
-    
       transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
-      if(transform.localScale.x <= .05f);
-    {
+      if (transform.localScale.x <= .05f);
+      {
   
         // Destroy object
         Destroy(gameObject);
-  }
-
-  }
+      }
+    }
           
     
-
+  }
 
    // {
 
