5a442267a43b7255080ba82dfd1b0970d28fd843
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-22 18:19:04 -0600

    hi

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+
+    [Header("Shape Object")]
+    public GameObject[] shapePrefabs;
+    [Header("Default spawn Delay Time")]
+    public float spawnDelay = 2f;
+     [Header("Default spawn Time")]
+     public float spawnTime = 3f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
